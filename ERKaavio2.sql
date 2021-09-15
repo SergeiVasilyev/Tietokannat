769416fdf9bc23b56b14bd4a6a7e7af6e31df2f9
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2021-09-15 10:06:30 +0300

    New folder Trash

---
+++ ERKaavio2.sql
@@ -0,0 +1,98 @@
+-- SQLite varten muokattu versio
+-- VARCHAR --> TEXT
+-- FLOAT --> REAL
+
+CREATE TABLE tuoteryhma (
+                tuoteryhma TEXT NOT NULL,
+                CONSTRAINT tuoteryhma_pk PRIMARY KEY (tuoteryhma)
+);
+
+
+CREATE TABLE tuote (
+                yksilointikoodi INTEGER NOT NULL,
+                nimike TEXT NOT NULL,
+                merkki TEXT NOT NULL,
+                malli TEXT NOT NULL,
+                paivavuokra REAL NOT NULL,
+                tuoteryhma TEXT NOT NULL,
+                CONSTRAINT tuote_pk PRIMARY KEY (yksilointikoodi)
+);
+
+
+CREATE TABLE yhteystyyppi (
+                yhteystyyppi TEXT NOT NULL,
+                CONSTRAINT yhteystyyppi_pk PRIMARY KEY (yhteystyyppi)
+);
+-- COMMENT ON COLUMN yhteystyyppi.yhteystyyppi IS 'Taulu on luotu vain k�ytt�liittym�� varten. Se est�� k�ytt�j� sy�tt�m�st� Yhteishenkil�-taulun muita kuin ennalta m��r�ttyj� arvoja';
+
+
+CREATE TABLE asiakas (
+                asiakasnumero INTEGER NOT NULL,
+                yritys TEXT NOT NULL,
+                laskutusosoite TEXT NOT NULL,
+                postinumero TEXT NOT NULL,
+                postitoimipaikka TEXT NOT NULL,
+                CONSTRAINT asiakas_pk PRIMARY KEY (asiakasnumero)
+);
+-- COMMENT ON COLUMN asiakas.laskutusosoite IS 'yrityksen laskujen katu- tai PL-osoite';
+
+
+CREATE TABLE vuokraus (
+                tapahtumaid INTEGER NOT NULL,
+                asiakasnumero INTEGER NOT NULL,
+                yksilointikoodi INTEGER NOT NULL,
+                alkamispaiva DATE NOT NULL,
+                paattymispaiva DATE NOT NULL,
+                CONSTRAINT vuokraus_pk PRIMARY KEY (tapahtumaid)
+);
+-- COMMENT ON COLUMN vuokraus.tapahtumaid IS 'Dummy id tulevia......';
+
+
+CREATE TABLE yhteyshenkilo (
+                yhteyshenkilo_id INTEGER NOT NULL,
+                yhteystyyppi TEXT NOT NULL,
+                etunimi TEXT NOT NULL,
+                sukunimi TEXT NOT NULL,
+                puhelin TEXT,
+                sahkoposti TEXT NOT NULL,
+                asiakasnumero INTEGER NOT NULL,
+                CONSTRAINT yhteyshenkilo_pk PRIMARY KEY (yhteyshenkilo_id)
+);
+-- COMMENT ON COLUMN yhteyshenkilo.yhteyshenkilo_id IS 'Dummy perusavain. Tulevaisuuden tarpeita varten';
+-- COMMENT ON COLUMN yhteyshenkilo.yhteystyyppi IS 'Taulu on luotu vain k�ytt�liittym�� varten. Se est�� k�ytt�j� sy�tt�m�st� Yhteishenkil�-taulun muita kuin ennalta m��r�ttyj� arvoja';
+
+
+ALTER TABLE tuote ADD CONSTRAINT tuoteryhma_tuote_fk
+FOREIGN KEY (tuoteryhma)
+REFERENCES tuoteryhma (tuoteryhma)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE vuokraus ADD CONSTRAINT tuote_vuokraus_fk
+FOREIGN KEY (yksilointikoodi)
+REFERENCES tuote (yksilointikoodi)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE yhteyshenkilo ADD CONSTRAINT yhteystyyppi_yhteyshenkilo_fk
+FOREIGN KEY (yhteystyyppi)
+REFERENCES yhteystyyppi (yhteystyyppi)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE yhteyshenkilo ADD CONSTRAINT asiakas_yhteyshenkilo_fk
+FOREIGN KEY (asiakasnumero)
+REFERENCES asiakas (asiakasnumero)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE vuokraus ADD CONSTRAINT asiakas_vuokraus_fk
+FOREIGN KEY (asiakasnumero)
+REFERENCES asiakas (asiakasnumero)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
